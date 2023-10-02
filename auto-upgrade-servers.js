e5ea4d3dada6b135575b5c4ba155a4a091945aaf
Leandro Lima <llimasilva@proton.me>
2023-10-02 18:48:02 -0300

    better scripts

--- auto-upgrade-servers.js
+++ auto-upgrade-servers.js
@@ -4,13 +4,14 @@ export async function main(ns) {
   let money = ns.getPlayer().money;
   const maxRam = 32768
   while (true) {
-    for (let i = 0; i < maxServers; i++) {
+    for (let i = 0; i <= maxServers; i++) {
       if (ns.serverExists("private-" + i)) {
         const ram = ns.getServerMaxRam("private-" + i) * 2
         const upgradeCost = ns.getPurchasedServerUpgradeCost("private-" + i, ram)
-        ns.print("upgrade cost:", upgradeCost)
+        ns.print("upgrade cost:", upgradeCost, "private-" + i )
         if (ram <= maxSizeRam && upgradeCost < money && ram <= maxRam) {
           ns.upgradePurchasedServer("private-" + i, ram)
+          ns.print("Upgraded: " + "private-" + i )
         }
       }
     }
